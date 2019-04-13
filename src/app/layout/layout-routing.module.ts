7893c1ceb517817f12a10fadbc91049bd608fc2f
Membriyin <membriyin@gmail.com>
2019-04-13 13:29:33 -0300

    Refactor Heroes Pge

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+            { path: 'heroes', loadChildren: './../heroes/heroes.module#HeroesModule' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
             { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
